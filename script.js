44819429d64b556011e00d68456c2552b5319f97
miiiwdy <fahmiwidyaa@gmail.com>
2023-11-22 15:16:13 +0700

    fix remove btn

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+  var rName = ["Asep", "Joko", "Yono", "Dono", "Iwan", "Yanto", "Iqbal"];
+  var randomNameIndex = Math.floor(Math.random() * rName.length);
+  var randomName = rName[randomNameIndex];
+  $("#rname").text("Bang " + randomName);
   var itemQuantity = {};
   var itemPrices = {};
 
@@ -36,9 +40,11 @@ $(document).ready(function () {
   }
 
   $(".iconplus").on("click", function () {
-    var $clickedItem = $(this).closest(".isi");
+    var $clickedItem = $(this).closest(".wadahItem");
+    var id = $clickedItem.data("qty");
     var itemName = $clickedItem.find("p").first().text().trim();
     var itemPriceText = $clickedItem.find("h1").text().trim();
+
     var itemPrice = parseFloat(
       itemPriceText.replace("Rp. ", "").replace(".", "").replace(",", ".")
     );
@@ -53,19 +59,19 @@ $(document).ready(function () {
         itemQuantity[formattedItemName]++;
       }
 
-      var isItemExist =
+      var wadahItemtemExist =
         $(".belanja .barang .detail-kiri p").filter(function () {
           return $(this).text().includes(itemName);
         }).length > 0;
 
-      if (!isItemExist) {
+      if (!wadahItemtemExist) {
         var newElement = `
-                <div class="barang">
+                <div class="barang" data-qty="${id}">
                     <div class="detail-kiri">
                         <p>${itemName} - <span id="stock_${formattedItemName}">${itemQuantity[formattedItemName]}</span></p>
-                        <p class="normal">Harga: ${itemPriceText}</p>
+                        <p class="normal">Harga: ${itemPriceText}/pcs</p>
                     </div>
-                    <button class="remove">
+                    <button class="remove" data-qty="${id}">
                         <span class="material-symbols-outlined">delete</span>
                     </button>
                 </div>
@@ -73,57 +79,21 @@ $(document).ready(function () {
 
         $(".belanja").append(newElement);
         updateTotalAfterQuantityChange();
+        $(`#stock_${formattedItemName}`).css("color", "#0062f0");
       } else {
         $("#stock_" + formattedItemName).text(itemQuantity[formattedItemName]);
         updateTotalAfterQuantityChange();
       }
     } else {
-      console.error("Harga item tidak valid");
-    }
-  });
-
-  $(".add_icon2").on("click", "#iconminus", function () {
-    var $item = $(this).closest(".barang");
-    var itemNameElement = $item.find(".detail-kiri p").first();
-    var itemName = itemNameElement.text().trim();
-    var itemPriceText = $item.find(".detail-kiri .normal").text().trim();
-    var itemPrice = parseFloat(
-      itemPriceText
-        .replace("Unit Price: Rp. ", "")
-        .replace(".", "")
-        .replace(",", ".")
-    );
-
-    if (!isNaN(itemPrice)) {
-      var totalPriceText = $("#totalPrice").text().trim();
-      var currentTotal =
-        parseFloat(
-          totalPriceText.replace("Rp. ", "").replace(".", "").replace(",", ".")
-        ) || 0;
-
-      var quantityElement = $(
-        "#stock_" + itemName.replace(/\s+/g, "_").toLowerCase()
-      );
-      var currentQuantity = parseInt(quantityElement.text());
-
-      if (currentQuantity > 1) {
-        currentQuantity--;
-        quantityElement.text(currentQuantity);
-        itemQuantity[itemName.replace(/\s+/g, "_").toLowerCase()]--;
-        updateTotalAfterQuantityChange();
-      } else {
-        delete itemQuantity[itemName.replace(/\s+/g, "_").toLowerCase()];
-        $item.remove();
-        $(this).find("#iconminus").hide();
-        updateTotalAfterQuantityChange();
-      }
-    } else {
-      console.error("Harga item tidak valid");
+      console.error("harga item ga valid");
     }
   });
 
   $(".belanja").on("click", ".remove", function () {
     var $item = $(this).closest(".barang");
+    var $id = $(this).data("qty");
+    element = $(".wadahItem").filter((index, value) => $(value).data("qty") == $id)[0];
+    var $element = $(element).find("#iconminus");
     var itemNameElement = $item.find(".detail-kiri p").first();
     var itemName = itemNameElement.text().split(" - ")[0].trim();
     var itemPriceText = $item.find(".detail-kiri .normal").text().trim();
@@ -141,62 +111,93 @@ $(document).ready(function () {
           totalPriceText.replace("Rp. ", "").replace(".", "").replace(",", ".")
         ) || 0;
 
-      var quantityElement = $(
-        "[id^='#stock_" + itemName.replace(/\s+/g, "_").toLowerCase() + "']"
-      ).remove();
-      var currentQuantity = parseInt(quantityElement.text());
+        var quantityElement = $(
+          "[id^='#stock_" + itemName.replace(/\s+/g, "_").toLowerCase() + "']"
+        ).remove();
+        var currentQuantity = parseInt(quantityElement.text());
 
       if (currentQuantity > 1) {
         currentQuantity--;
         quantityElement.text(currentQuantity);
         itemQuantity[itemName.replace(/\s+/g, "_").toLowerCase()]--;
         updateTotalAfterQuantityChange();
-      } else if (currentQuantity === 1) {
-        currentQuantity--;
-        quantityElement.text(currentQuantity);
-        itemQuantity[itemName.replace(/\s+/g, "_").toLowerCase()]--;
-        updateTotalAfterQuantityChange();
       } else {
         delete itemQuantity[itemName.replace(/\s+/g, "_").toLowerCase()];
+        $element.hide();
+                        console.log("iconminus id removed: "+$id);
         $item.remove();
-        $(this).find("#iconminus").hide();
         updateTotalAfterQuantityChange();
       }
     } else {
-      console.error("Harga item tidak valid");
+      console.error("harga item ga valid");
     }
   });
 
+  $(".wadahItem").on("click", "#iconminus", function () {
+    var $item = $(this).closest(".barang");
+    var $clickedItem = $(this).closest(".wadahItem");
+    var id = $clickedItem.data("qty");
+    var element = $(".wadahItem").filter((index, value) => $(value).data("qty") == id)[0];
+    var $element = $(element).find("#iconminus");
+    var itemName = $clickedItem.find("p").first().text().trim();
+    var itemPriceText = $clickedItem.find("h1").text().trim();
+
+    var itemPrice = parseFloat(
+        itemPriceText.replace("Rp. ", "").replace(".", "").replace(",", ".")
+    );
+
+    if (!isNaN(itemPrice)) {
+        var formattedItemName = itemName.replace(/\s+/g, "_").toLowerCase();
+
+        if (itemQuantity[formattedItemName] && itemQuantity[formattedItemName] > 0) {
+            itemQuantity[formattedItemName]--;
+
+            if (itemQuantity[formattedItemName] === 0) {
+                $element.hide();
+                $(".belanja .barang[data-qty='" + id + "']").remove();
+                console.log("iconminus id removed: "+id);
+                $("#stock_" + formattedItemName).text(itemQuantity[formattedItemName]);
+                updateTotalAfterQuantityChange();
+            } else {
+                $("#stock_" + formattedItemName).text(itemQuantity[formattedItemName]);
+                updateTotalAfterQuantityChange();
+            }
+        } 
+        else {
+            console.error("quantity ga valid");
+        }
+    } else {
+        console.error("harga item ga valid");
+    }
+});
+
   $("#bayar").click(function () {
-    console.log("Tombol 'bayar' ditekan!");
     var kiriID = $(".kiri").attr("id");
-    console.log("ID kiri:", kiriID);
     window.print();
   });
 
   $(document).keydown(function (event) {
     if (event.ctrlKey && event.key === "p") {
-      console.log("Tombol 'Ctrl + P' ditekan. Melakukan pencetakan...");
-
       event.preventDefault();
       window.print();
     }
   });
 
   $("#searchIcon").on("click", function () {
-    $("#searchInput").fadeToggle("fast");
+    $("#searchInput").css("border-radius", "20px")
+    $("#searchInput").fadeToggle("slow");
     $("#searchInput").focus();
   });
 
   $("#searchInput").on("keyup", function () {
     var value = $(this).val().toLowerCase();
-    $(".isi").filter(function () {
+    $(".wadahItem").filter(function () {
       $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
     });
   });
 
   $("#searchInput").on("focusout", function () {
-    $(this).fadeOut("fast");
+    $(this).fadeOut("slow");
   });
 
   $(document).on("keydown", function (event) {
@@ -205,36 +206,28 @@ $(document).ready(function () {
     }
   });
 
-  $("#sortByName").on("click", function () {
-    var items = $(".isi").detach();
-    let ascending = true;
-
-    items.sort(function (a, b) {
-      var nameA = $(a).find("p").text().toUpperCase();
-      var nameB = $(b).find("p").text().toUpperCase();
-
-      return ascending
-        ? nameA.localeCompare(nameB)
-        : nameB.localeCompare(nameA);
-    });
-
-    ascending = !ascending;
-
-    $(".kanan > section").html(items);
-  });
-
-  //otak-atik hover
+  var hover = false;
   $(".iconplus").hover(
     function () {
-      $(this).closest(".isi").addClass("hovered");
+      $(this).closest(".wadahItem").addClass("hovered");
+      hover = true;
     },
     function () {
-      $(this).closest(".isi").removeClass("hovered");
+      $(this).closest(".wadahItem").removeClass("hovered");
+      hover = false;
     }
   );
 
-  //iconminus
-  $(".isi").on("click", function () {
-    $(this).find("#iconminus").show();
+  $(".wadahItem").on("click", function () {
+    let iconminus = $(this).find("#iconminus");
+    if (!hover) {
+    } else {
+      iconminus.show();
+    }
+  });
+  $(".pay").on("click", function () {
+    let kotak = $(this).nextAll(".tengah").first();
+    kotak.fadeIn("slow");
+    kotak.show();
   });
 });
